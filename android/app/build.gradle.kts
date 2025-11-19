09eed6b4399aedf6ba5e149af390c913fa8e2c0e
seli-hom <efremselihom1@gmail.com>
2025-11-19 11:23:17 -0500

    added db library of ruina with its keys google services json

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+    //Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -41,4 +43,14 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bookshop/loginpage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCjexEQCUZ3OhMj-VyXJcYTV7clWJclu6w",
+          appId: "464024739524",
+          messagingSenderId: "1:464024739524:android:7bd3f8116a399a5672f391",
+          projectId: "library-of-ruina"));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
