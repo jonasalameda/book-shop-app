679ae7a160b6a0dcbb77e4d2a14a15c2cb4ecb3b
seli-hom <efremselihom1@gmail.com>
2025-11-18 22:01:13 -0500

    login file just the look did not doo the routse and app bar file started

---
+++ lib/appBar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+enum Options{search, myCart, wishList, logout, filter}
+
+var _popUpMenuIndex = 0;
+//let the App Bar control the height of the menu
+var appBarHeight = AppBar().preferredSize.height;
+
+//lets build the appBar container
+
+AppBar buildAppBar(){
+  return AppBar(
+    title: Text('Pop Up Menu',
+      style: TextStyle(color: Colors.white, fontSize: 30),
+    ),
+    backgroundColor: Colors.pink,
+    actions: [
+      PopupMenuButton(
+        onSelected: (value){
+          _onMenuItemsSelected(value as int);
+        },
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(8.0),
+                topLeft: Radius.circular(8.0),
+                bottomRight: Radius.circular(8.0),
+                topRight: Radius.circular(8.0)
+            )
+        ),
+        itemBuilder: (context) => [
+          _buildPopUpItem('Search', Icons.search, Options.search.index),
+          _buildPopUpItem('Filter', Icons.filter_alt_outlined, Options.filter.index),
+          _buildPopUpItem('My saved', Icons.favorite, Options.wishList.index),
+          _buildPopUpItem('My cart', Icons.shopping_cart, Options.myCart.index),
+          _buildPopUpItem('Log Out', Icons.exit_to_app, Options.logout.index),
+        ],
+      )
+    ],
+  );
+}
+
+PopupMenuItem _buildPopUpItem(String text, IconData iconData, int position){
+  return PopupMenuItem(
+    value: position,
+    child: Row(
+      children: [
+        Icon(iconData, color: Colors.brown,),
+        Text(text),
+      ],
+    ),
+  );
+}
+
+
+//TODO: import page routes from files for item selection
+_onMenuItemsSelected(int value){
+  _popUpMenuIndex = value;
+
+  if(value ==  Options.search.index){
+    //change page to found value
+  } else if(value ==  Options.filter.index){
+    //filer method and refresh page
+  }  else if(value ==  Options.wishList.index){
+    //change page to saved books
+  }  else if(value ==  Options.myCart.index){
+    //switch to cart page
+  } else if(value ==  Options.logout.index){
+    //pop page and go back to splash screen
+  }
+}
---
+++ lib/loginpage.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:bookshop/appBar.dart';
+
+class LogInPage extends StatefulWidget {
+  const LogInPage({super.key});
+
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  final _userNameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _obscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      backgroundColor: Colors.brown.shade200,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(50.0),
+              child: Container(
+                color: Colors.yellow.shade50,
+                child: TextField(
+                  controller: _userNameController,
+                  decoration: InputDecoration(labelText: 'Password:'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(50.0),
+              child: Container(
+                color: Colors.yellow.shade50,
+                child: TextField(
+                  controller: _passwordController,
+                  obscureText: _obscured,
+                  decoration: InputDecoration(
+                    labelText: 'Password:',
+                    icon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _obscured = !_obscured;
+                        });
+                      },
+                      icon: Icon(Icons.remove_red_eye, color: Colors.brown),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 35),
+            Row(
+              children: [
+                SizedBox(width: 30,),
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //TODO: Routse to main products list page
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.brown.shade500),),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),),
+                SizedBox(width: 30,)
+              ],
+            )
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
