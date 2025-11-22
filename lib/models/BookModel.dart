f80549bab73667971f06cc430a36e668f39a9472
jonas <125613982+jonasalameda@users.noreply.github.com>
2025-11-21 20:06:08 -0500

    feat: implement crud on book model

--- lib/models/BookModel.dart
+++ lib/models/BookModel.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:bookshop/views/loginpage.dart';
+
 /**
  * Here lies all functionalities/back-end logic of Book class
  */
-class _BookModel {
-  int book_id;
+class BookModel {
+  String book_id;
   String book_name;
   String author;
   String country;
@@ -12,7 +16,11 @@ class _BookModel {
   double price;
   bool available;
 
-  _BookModel(
+  final CollectionReference books = FirebaseFirestore.instance.collection(
+    'Books',
+  );
+
+  BookModel(
     this.book_id,
     this.book_name,
     this.author,
@@ -23,4 +31,34 @@ class _BookModel {
     this.price,
     this.available,
   );
+
+  Future<void> addBook() async {
+    await books.add({
+      'name': book_name,
+      'author': author,
+      'country': country,
+      'genres': genres,
+      'description': description,
+      'quantity': quantity,
+      'price': price,
+      'available': available,
+    });
+  }
+
+  Future<void> deleteBook(String id) async {
+    await books.doc(id).delete();
+  }
+
+  Future<void> updateBook(id) async {
+    await books.doc(id).update({
+      'name': book_name,
+      'author': author,
+      'country': country,
+      'genres': genres,
+      'description': description,
+      'quantity': quantity,
+      'price': price,
+      'available': available,
+    });
+  }
 }
