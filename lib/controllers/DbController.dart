58681f450e56181bc3a1734b8b61b5d19de5d2f3
Mariam Salim <6229528@edu.vaniercollege.qc.ca>
2025-11-21 19:35:04 -0500

    DbController small update

--- lib/controllers/DbController.dart
+++ lib/controllers/DbController.dart
@@ -1,8 +1,29 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mvc_application/controller.dart' show ControllerMVC;
 // import '../models/BookModel.dart';
 
-Future<void> addColumn(CollectionReference collection, newObject) async {
-    await collection.add(newObject.toMap());
+/// Adds a column from the input reference collection with the inserted newObject.
+/// The object must have a toMap function.
+/// The function will return false in case of db failure
+Future<bool> addColumn(CollectionReference collection, newObject) async {
+    try {
+        await collection.add(newObject.toMap());
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+/// Deletes a column from the input reference collection where the id matches.
+/// The function will return false in case of db failure.
+Future<bool> deleteColumn(String id, CollectionReference collection) async {
+    try {
+        await collection.doc(id).delete();
+        return true;
+    } catch(e) {
+        return false;
+    }
 }
 
