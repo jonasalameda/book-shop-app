5f00919fe9aedbf9b3db19e75c48d6639e3aa24a
Mariam Salim <6229528@edu.vaniercollege.qc.ca>
2025-11-24 14:06:36 -0500

    Removed useless files and added remaining views and routees

--- lib/controllers/CartController.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mvc_application/controller.dart' show ControllerMVC;
-
-/// CartController communicates with the view to render logic on the code
-class _CartController extends ControllerMVC {
-}
