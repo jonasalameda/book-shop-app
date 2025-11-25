267ea284b10fe12e988cd941065dfcd09c610c46
jonas <125613982+jonasalameda@users.noreply.github.com>
2025-11-25 17:38:41 -0500

    fix: fix Database Controller issues

--- lib/controllers/DbController.dart
+++ lib/controllers/DbController.dart
@@ -4,23 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mvc_application/controller.dart' show ControllerMVC;
 
 //TODO: Initialize all collections/tables
-<<<<<<< HEAD
-class BookDB{
-    final CollectionReference books =
-    FirebaseFirestore.instance.collection('Books');
-
-    void initializeDB() async{
-        WidgetsFlutterBinding.ensureInitialized();
-        await Firebase.initializeApp(
-            options: const FirebaseOptions(
-                apiKey: "AIzaSyBbuPvGLzeKv0pXmx00hlXySPxT7F8ZQcE",
-                appId: "530607033314",
-                messagingSenderId: "1:530607033314:android:f0812a8ac5623e81bd47bb",
-                projectId: "book-shop-cdfd8",
-            ),
-        );
-    }
-=======
 class BookDB {
   final CollectionReference books =
       FirebaseFirestore.instance.collection('Books');
@@ -39,51 +22,23 @@ class BookDB {
       ),
     );
   }
->>>>>>> 5f00919fe9aedbf9b3db19e75c48d6639e3aa24a
 }
 
 /// Adds a column from the input reference collection with the inserted newObject.
 /// The object must have a toMap function.
 /// The function will return false in case of db failure
 Future<bool> addRow(CollectionReference collection, newObject) async {
-<<<<<<< HEAD
-    try {
-        await collection.add(newObject.toMap());
-        return true;
-    } catch(e) {
-        return false;
-    }
-=======
   try {
     await collection.add(newObject.toMap());
     return true;
   } catch (e) {
     return false;
   }
->>>>>>> 5f00919fe9aedbf9b3db19e75c48d6639e3aa24a
 }
 
 /// Deletes a column from the input reference collection where the id matches.
 /// The function will return false in case of db failure.
 Future<bool> deleteRow(String id, CollectionReference collection) async {
-<<<<<<< HEAD
-    try {
-        await collection.doc(id).delete();
-        return true;
-    } catch(e) {
-        return false;
-    }
-}
-
-
-Future<bool> updateRow(String id, CollectionReference collection, updatedObject) async {
-    try {
-        await collection.doc(id).update(updatedObject.toMap());
-        return true;
-    } catch(e) {
-        return false;
-    }
-=======
   try {
     await collection.doc(id).delete();
     return true;
@@ -100,5 +55,4 @@ Future<bool> updateRow(
   } catch (e) {
     return false;
   }
->>>>>>> 5f00919fe9aedbf9b3db19e75c48d6639e3aa24a
 }
